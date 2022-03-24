c5d4a6f70b88f22027ff57be3e1b4e9e1d5e05b7
Neville Walo <neville.walo@gmail.com>
2022-03-24 11:52:46 +0100

    added masking

--- stable_baselines3/common/policies.py
+++ stable_baselines3/common/policies.py
@@ -617,7 +617,7 @@ class ActorCriticPolicy(BasePolicy):
         action_logits = self.action_net(latent_pi)
         return values, action_logits
 
-    def _get_action_dist_from_latent(self, latent_pi: th.Tensor) -> Distribution:
+    def _get_action_dist_from_latent(self, latent_pi: th.Tensor, obs: th.Tensor) -> Distribution:
         """
         Retrieve action distribution given the latent codes.
 
@@ -625,6 +625,44 @@ class ActorCriticPolicy(BasePolicy):
         :return: Action distribution
         """
         mean_actions = self.action_net(latent_pi)
+
+        assert obs.size(1) == 118
+        assert mean_actions.size(1) == 22
+
+        has_boost = obs[:, 13] > 0.0
+        on_ground = obs[:, 14]
+        has_flip = obs[:, 15]
+
+        not_on_ground = th.logical_not(on_ground)
+        mask = th.ones_like(mean_actions, dtype=th.bool)
+
+        # mask[:, 0:3] = 1.0  # Throttle, always possible
+        # mask[:, 3:8] = 1.0  # Steer yaw, always possible
+        # mask[:, 8:13] = 1.0  # pitch, not on ground but (flip resets, walldashes)
+        # mask[:, 13:16] = 1.0  # roll, not on ground
+        # mask[:, 16:18] = 1.0  # jump, has flip (turtle)
+        # mask[:, 18:20] = 1.0  # boost, boost > 0
+        # mask[:, 20:22] = 1.0  # Handbrake, at least one wheel ground (not doable)
+
+        mask[:, 0] = on_ground  # throttle -1
+        # mask[:, 2] = on_ground  # throttle 1
+
+        mask[:, 8] = not_on_ground  # pitch -1
+        mask[:, 9] = not_on_ground  # pitch -0.5
+        mask[:, 11] = not_on_ground  # pitch 0.5
+        mask[:, 12] = not_on_ground  # pitch 1.0
+
+        mask[:, 13] = not_on_ground  # roll -1
+        mask[:, 15] = not_on_ground  # roll 1
+
+        mask[:, 17] = has_flip  # Jump
+        mask[:, 19] = has_boost  # boost
+
+        mask[:, 21] = on_ground  # Handbrake
+
+        HUGE_NEG = th.tensor(-1e8, dtype=mean_actions.dtype, device=self.device)
+
+        mean_actions = th.where(mask, mean_actions, HUGE_NEG)
 
         if isinstance(self.action_dist, DiagGaussianDistribution):
             return self.action_dist.proba_distribution(mean_actions, self.log_std)
